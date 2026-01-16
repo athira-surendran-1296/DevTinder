119609b9e48992ba3f561e98160f95b702b7ed1c
athira <athirasurendran1296@gmail.com>
2026-01-16 20:31:24 +0530

    Remove secret data from GIT

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-
-const DB_CONNECTION_STRING = "mongodb+srv://athirasurendran1296:athirasurendran1296@mynodecluster.wj5cm9u.mongodb.net/";
-const DB_NAME = "devTinder";
+const { DB_CONNECTION_STRING, DB_NAME } = require("../../env");
 
 const connectDB = async () => {
     // Connect to clusture - Returns a promise
