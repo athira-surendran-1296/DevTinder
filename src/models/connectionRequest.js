81c8946ca92b0ea3c6524df9070cc39857b73d59
athira <athirasurendran1296@gmail.com>
2026-01-27 20:14:01 +0530

    Logical DB Query & Compound Indexes

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const connectionRequestSchema = new Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type.`
+      }
+    },
+  }, 
+  { 
+    timestamps: true 
+  }
+);
+
+// Pre save --> Always called before save. Dont use arrow function 
+connectionRequestSchema.pre("save", async function () {
+  const connectionRequest = this;
+  // use equals method as id is object id and not just string
+  if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send request to yourself!");
+  }
+});
+
+// Compound index
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 }); // 1 is ascending order
+
+const ConnectionRequest = mongoose.model("ConnectionRequest", connectionRequestSchema);
+
+module.exports = ConnectionRequest;
